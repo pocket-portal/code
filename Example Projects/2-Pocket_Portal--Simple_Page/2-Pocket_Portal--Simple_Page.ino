8c5b0d750849a3f64afe4f26254eef0cf285766b
candideu <55474996+candideu@users.noreply.github.com>
2024-05-04 00:54:25 -0400

    Update to correct SSID

--- Example Projects/2-Pocket_Portal--Simple_Page/2-Pocket_Portal--Simple_Page.ino
+++ Example Projects/2-Pocket_Portal--Simple_Page/2-Pocket_Portal--Simple_Page.ino
@@ -16,7 +16,7 @@ const byte DNS_PORT = 53;
 ESP8266WebServer server(80);
 
 #ifndef STASSID
-#define STASSID "Digital Colouring Book" // ✨ set your network name here ✨
+#define STASSID "Come in, grab a seat" // ✨ set your network name here ✨
 #endif
 
 IPAddress apIP(192, 168, 4, 1);
